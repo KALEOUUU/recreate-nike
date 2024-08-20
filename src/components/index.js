069151e621613864aac908847eca3fad45a16198
kaleee <shofiyullohkamil01@gmail.com>
2024-08-20 14:18:57 +0700

    first-commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+import Button from "./Button";
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
