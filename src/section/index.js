2c8bd901711bd626213dc415a752a7dbe09311c9
kaleee <shofiyullohkamil01@gmail.com>
2024-08-20 14:42:49 +0700

    fix-code

--- src/section/index.js
+++ src/section/index.js
@@ -7,13 +7,11 @@ import SpecialProducts from './SpecialProducts';
 import Subscribe from './Subscribe';
 import SuperQuality from './SuperQuality'; 
 
-export {
-    Hero,
-    CustomerReviews,
-    Footer,
-    PopularProduct,
-    Services,
-    SpecialProducts,
-    Subscribe,
-    SuperQuality
-};
+export { default as PopularProducts } from './PopularProducts';
+export { default as CustomerReviews } from './CustomerReviews';
+export { default as Hero } from './Hero';
+export { default as Footer } from './Footer';
+export { default as SpecialProducts } from './SpecialProducts';
+export { default as Subscribe } from './Subscribe';
+export { default as Services } from './Services';
+export { default as SuperQuality } from './SuperQuality';
